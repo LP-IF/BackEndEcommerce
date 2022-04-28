5964e4fa9aab58799c58309ad8856bb9bb5ed3ed
valescaalvesc <valescaalvess@gmail.com>
2022-04-28 11:09:18 -0300

    Criação da classe Cartão

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/Cartao.java
@@ -0,0 +1,26 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCartao;
+    private String nomeTitular;
+    private Integer numCartao;
+    private Date validade;
+    private String cvv;
+
+    @ManyToOne
+    private Usuario usuario;
+}
