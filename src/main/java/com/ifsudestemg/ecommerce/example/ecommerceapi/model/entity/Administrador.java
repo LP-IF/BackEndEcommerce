9166527323d5076d724e56b9e94b45d08f844755
valescaalvesc <valescaalvess@gmail.com>
2022-04-28 10:29:52 -0300

    Classe Administrador

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/Administrador.java
@@ -0,0 +1,23 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Administrador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAdministrador;
+    private Date dataNascimento;
+    private String cpf;
+}
