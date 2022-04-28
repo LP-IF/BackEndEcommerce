36946aa0db9627f00d015b148e69f960a40788ad
valescaalvesc <valescaalvess@gmail.com>
2022-04-28 10:57:32 -0300

    Classe Cliente

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/Cliente.java
@@ -0,0 +1,18 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Cliente extends Usuario {
+    private Date dataNascimento;
+    private String cpf;
+}
