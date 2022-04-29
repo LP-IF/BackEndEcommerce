c71ae800ab5ffbc2a869b9d05fb81a9e2e8ff0c8
valescaalvesc <valescaalvess@gmail.com>
2022-04-29 15:18:53 -0300

    Criação da classe PagamentoVista

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/PagamentoVista.java
@@ -0,0 +1,18 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PagamentoVista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPagamentoVista;
+}
