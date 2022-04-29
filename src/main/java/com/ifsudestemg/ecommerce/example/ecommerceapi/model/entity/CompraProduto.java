8c4b666821b655de5e2aacce88b9a013401aac18
valescaalvesc <valescaalvess@gmail.com>
2022-04-29 14:35:33 -0300

    Classe CompraProduto

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/CompraProduto.java
@@ -0,0 +1,20 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CompraProduto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCompraProduto;
+    private Date diaDeEntrega;
+}
