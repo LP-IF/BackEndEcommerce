cd0f2d0a2dcd6476f5bc927b02de2f3f14a3fd55
valescaalvesc <valescaalvess@gmail.com>
2022-04-29 15:15:08 -0300

    Criação da classe Pagamento

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/Pagamento.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/Pagamento.java
@@ -1,2 +1,20 @@
-package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;public class Pagamento {
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPagamento;
+    private Double preco;
+    private Double desconto;
 }
