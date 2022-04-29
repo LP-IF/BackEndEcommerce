0fd038a036d6db65397d7b3b8be543d53976aa3e
valescaalvesc <valescaalvess@gmail.com>
2022-04-29 15:02:27 -0300

    Atualização ItensCarrinho

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/ItensCarrinho.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/model/entity/ItensCarrinho.java
@@ -1,2 +1,23 @@
-package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;public class ItensCarrinho {
+package com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ItensCarrinho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idItensCarrinho;
+    private String nomeProduto;
+    private Double precoUnit;
+    private Double precoTotal;
+    private Integer quant;
 }
