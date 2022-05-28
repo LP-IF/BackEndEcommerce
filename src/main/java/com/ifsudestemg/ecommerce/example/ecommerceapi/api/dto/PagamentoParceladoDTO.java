d87d540a90469a6cc494a0bb750886085b77a91d
valescaalvesc <valescaalvess@gmail.com>
2022-05-28 10:05:56 -0300

    PagamentoParceladoDTO

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/dto/PagamentoParceladoDTO.java
@@ -0,0 +1,23 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto;
+
+import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.PagamentoParcelado;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PagamentoParceladoDTO {
+    private Integer parcelas;
+    private Double juros;
+
+    public static PagamentoParceladoDTO create(PagamentoParcelado pagamentoParcelado) {
+        ModelMapper modelMapper = new ModelMapper();
+        PagamentoParceladoDTO dto = modelMapper.map(pagamentoParcelado, PagamentoParceladoDTO.class);
+        return dto;
+    }
+}
