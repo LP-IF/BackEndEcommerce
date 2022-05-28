af0cabe710e3bb98ee3a673216041e9c4b233429
valescaalvesc <valescaalvess@gmail.com>
2022-05-28 09:58:17 -0300

    ItemVendaDTO

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/dto/ItemVendaDTO.java
@@ -0,0 +1,25 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto;
+
+import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.ItemVenda;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ItemVendaDTO {
+    private Long Id;
+    private Double precoUnit;
+    private Double precoTotal;
+    private Integer quant;
+
+    public static ItemVendaDTO create(ItemVenda itemVenda){
+        ModelMapper modelMapper = new ModelMapper();
+        ItemVendaDTO dto = modelMapper.map(itemVenda, ItemVendaDTO.class);
+        return dto;
+    }
+}
