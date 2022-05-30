61ad3267ba88c4f7270c8380c45063d10b3d4d1c
Gabriel Faria <gabriel.selu@gmail.com>
2022-05-30 20:07:03 -0300

    CategoriaDTO

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/dto/CategoriaDTO.java
@@ -0,0 +1,22 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto;
+
+import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.Cartao;
+import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaDTO {
+    private Long Id;
+    private String nome;
+
+    public static CategoriaDTO create(Categoria categoria) {
+        ModelMapper modelMapper = new ModelMapper();
+        CategoriaDTO dto = modelMapper.map(categoria, CategoriaDTO.class);
+        return dto;
+    }
+}
