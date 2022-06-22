e40d4d0e366e7649f26ad589dc755116bf0c0935
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-22 18:06:04 -0300

    adiciona metodos de post, put, delete e método de validação a CategoriaController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/CategoriaController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/CategoriaController.java
@@ -2,14 +2,13 @@ package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.CategoriaDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.Categoria;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.CategoriaService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,4 +34,49 @@ public class CategoriaController {
         }
         return ResponseEntity.ok(categoria.map(CategoriaDTO::create));
     }
+
+    @PostMapping()
+    public ResponseEntity post(CategoriaDTO dto) {
+        try {
+            Categoria categoria = converter(dto);
+            categoria = service.salvar(categoria);
+            return new ResponseEntity(categoria, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, CategoriaDTO dto) {
+        if (!service.getCategoriaById(id).isPresent()) {
+            return new ResponseEntity("Categoria não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Categoria categoria = converter(dto);
+            categoria.setId(id);
+            service.salvar(categoria);
+            return ResponseEntity.ok(categoria);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Categoria> categoria = service.getCategoriaById(id);
+        if (!categoria.isPresent()) {
+            return new ResponseEntity("Categoria não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(categoria.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+        public Categoria converter(CategoriaDTO dto) {
+            ModelMapper modelMapper = new ModelMapper();
+            Categoria categoria = modelMapper.map(dto, Categoria.class);
+            return categoria;
+        }
 }
