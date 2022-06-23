0a41f86868169750510aea27a17672dfc5d1b74c
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-23 13:49:42 -0300

    adiciona métodos de post, put e delete ao CupomController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/CupomController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/CupomController.java
@@ -1,14 +1,13 @@
 package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.CupomDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.Cupom;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.CupomService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +32,51 @@ public class CupomController {
             return new ResponseEntity("Cupom não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(cupom.map(CupomDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(CupomDTO dto) {
+        try {
+            Cupom cupom = converter(dto);
+            cupom = service.salvar(cupom);
+            return new ResponseEntity(cupom, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, CupomDTO dto) {
+        if (!service.getCupomById(id).isPresent()) {
+            return new ResponseEntity("Cupom não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Cupom cupom = converter(dto);
+            cupom.setId(id);
+            service.salvar(cupom);
+            return ResponseEntity.ok(cupom);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Cupom> cupom = service.getCupomById(id);
+        if (!cupom.isPresent()) {
+            return new ResponseEntity("Cupom não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(cupom.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Cupom converter(CupomDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Cupom cupom = modelMapper.map(dto, Cupom.class);
+        return cupom;
     }
 }
