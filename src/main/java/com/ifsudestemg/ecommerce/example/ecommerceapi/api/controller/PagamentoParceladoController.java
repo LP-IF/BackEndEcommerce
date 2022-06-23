468e5fe2436aeca9d1b355a2a9489f27a168e070
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-23 13:51:46 -0300

    adiciona métodos de post, put e delete ao PagamentoVistaController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/PagamentoParceladoController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/PagamentoParceladoController.java
@@ -1,14 +1,13 @@
 package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.PagamentoParceladoDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.PagamentoParcelado;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.PagamentoParceladoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +32,51 @@ public class PagamentoParceladoController {
             return new ResponseEntity("Pagamento Parcelado não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(pagamentoParcelado.map(PagamentoParceladoDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(PagamentoParceladoDTO dto) {
+        try {
+            PagamentoParcelado pagamentoParcelado = converter(dto);
+            pagamentoParcelado = service.salvar(pagamentoParcelado);
+            return new ResponseEntity(pagamentoParcelado, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, PagamentoParceladoDTO dto) {
+        if (!service.getPagamentoParceladoById(id).isPresent()) {
+            return new ResponseEntity("PagamentoParcelado não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            PagamentoParcelado pagamentoParcelado = converter(dto);
+            pagamentoParcelado.setId(id);
+            service.salvar(pagamentoParcelado);
+            return ResponseEntity.ok(pagamentoParcelado);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<PagamentoParcelado> pagamentoParcelado = service.getPagamentoParceladoById(id);
+        if (!pagamentoParcelado.isPresent()) {
+            return new ResponseEntity("PagamentoParcelado não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(pagamentoParcelado.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public PagamentoParcelado converter(PagamentoParceladoDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        PagamentoParcelado pagamentoParcelado = modelMapper.map(dto, PagamentoParcelado.class);
+        return pagamentoParcelado;
     }
 }
