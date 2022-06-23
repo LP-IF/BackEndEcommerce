8d0de653125290b595a633eee1e220c23fdbc320
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-23 13:50:01 -0300

    adiciona métodos de post, put e delete ao EnderecoController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/EnderecoController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/EnderecoController.java
@@ -2,14 +2,13 @@ package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.EnderecoDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.Endereco;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.EnderecoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,5 +33,50 @@ public class EnderecoController {
             return new ResponseEntity("Endereco não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(endereco.map(EnderecoDTO::create));
+    }
+    @PostMapping()
+    public ResponseEntity post(EnderecoDTO dto) {
+        try {
+            Endereco endereco = converter(dto);
+            endereco = service.salvar(endereco);
+            return new ResponseEntity(endereco, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, EnderecoDTO dto) {
+        if (!service.getEnderecoById(id).isPresent()) {
+            return new ResponseEntity("Endereco não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Endereco endereco = converter(dto);
+            endereco.setId(id);
+            service.salvar(endereco);
+            return ResponseEntity.ok(endereco);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Endereco> endereco = service.getEnderecoById(id);
+        if (!endereco.isPresent()) {
+            return new ResponseEntity("Endereco não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(endereco.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Endereco converter(EnderecoDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Endereco endereco = modelMapper.map(dto, Endereco.class);
+        return endereco;
     }
 }
