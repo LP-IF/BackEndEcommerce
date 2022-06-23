0495c42d8d6b472f5e4ed7a707f8ae2bfee613a4
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-23 13:51:26 -0300

    adiciona métodos de post, put e delete ao ItemVendaController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/ItemVendaController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/ItemVendaController.java
@@ -2,14 +2,13 @@ package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.ItemVendaDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.ItemVenda;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.ItemVendaService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,5 +33,51 @@ public class ItemVendaController {
             return new ResponseEntity("Item Venda não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(itemVenda.map(ItemVendaDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(ItemVendaDTO dto) {
+        try {
+            ItemVenda itemVenda = converter(dto);
+            itemVenda = service.salvar(itemVenda);
+            return new ResponseEntity(itemVenda, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, ItemVendaDTO dto) {
+        if (!service.getItemVendaById(id).isPresent()) {
+            return new ResponseEntity("ItemVenda não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            ItemVenda itemVenda = converter(dto);
+            itemVenda.setId(id);
+            service.salvar(itemVenda);
+            return ResponseEntity.ok(itemVenda);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<ItemVenda> itemVenda = service.getItemVendaById(id);
+        if (!itemVenda.isPresent()) {
+            return new ResponseEntity("ItemVenda não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(itemVenda.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public ItemVenda converter(ItemVendaDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        ItemVenda itemVenda = modelMapper.map(dto, ItemVenda.class);
+        return itemVenda;
     }
 }
