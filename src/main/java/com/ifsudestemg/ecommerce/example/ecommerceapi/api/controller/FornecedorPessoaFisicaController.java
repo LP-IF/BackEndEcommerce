30e709464e802cc014b14a651a5abd2a07084e0f
Gabriel Faria <gabriel.selu@gmail.com>
2022-06-23 13:50:35 -0300

    adiciona métodos de post, put e delete ao FornecedorPessoaFisicaController

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/FornecedorPessoaFisicaController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/FornecedorPessoaFisicaController.java
@@ -1,14 +1,13 @@
 package com.ifsudestemg.ecommerce.example.ecommerceapi.api.controller;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.FornecedorPessoaFisicaDTO;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.FornecedorPessoaFisica;
+import com.ifsudestemg.ecommerce.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.service.FornecedorPessoaFisicaService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +32,51 @@ public class FornecedorPessoaFisicaController {
             return new ResponseEntity("Fornecedor Pessoa Fisica não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(fornecedorPessoaFisica.map(FornecedorPessoaFisicaDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(FornecedorPessoaFisicaDTO dto) {
+        try {
+            FornecedorPessoaFisica fornecedorPessoaFisica = converter(dto);
+            fornecedorPessoaFisica = service.salvar(fornecedorPessoaFisica);
+            return new ResponseEntity(fornecedorPessoaFisica, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, FornecedorPessoaFisicaDTO dto) {
+        if (!service.getFornecedorPessoaFisicaById(id).isPresent()) {
+            return new ResponseEntity("FornecedorPessoaFisica não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            FornecedorPessoaFisica fornecedorPessoaFisica = converter(dto);
+            fornecedorPessoaFisica.setId(id);
+            service.salvar(fornecedorPessoaFisica);
+            return ResponseEntity.ok(fornecedorPessoaFisica);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<FornecedorPessoaFisica> fornecedorPessoaFisica = service.getFornecedorPessoaFisicaById(id);
+        if (!fornecedorPessoaFisica.isPresent()) {
+            return new ResponseEntity("FornecedorPessoaFisica não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(fornecedorPessoaFisica.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public FornecedorPessoaFisica converter(FornecedorPessoaFisicaDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        FornecedorPessoaFisica fornecedorPessoaFisica = modelMapper.map(dto, FornecedorPessoaFisica.class);
+        return fornecedorPessoaFisica;
     }
 }
