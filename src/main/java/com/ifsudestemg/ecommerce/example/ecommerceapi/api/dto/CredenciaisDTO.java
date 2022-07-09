3dd25084afae203e975631576792af904c770753
Gabriel Faria <gabriel.selu@gmail.com>
2022-07-09 20:44:44 -0300

    cria CredenciaisDTO

---
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/dto/CredenciaisDTO.java
@@ -0,0 +1,11 @@
+package com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+    private String login;
+    private String senha;
+}
