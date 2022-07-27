6aacfed90928af050a1d290ece7f8d5e922b85f5
Gabriel Faria <gabriel.selu@gmail.com>
2022-07-27 14:10:36 -0300

    adiciona swagger com especificações

--- src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/PagamentoParceladoController.java
+++ src/main/java/com/ifsudestemg/ecommerce/example/ecommerceapi/api/controller/PagamentoParceladoController.java
@@ -3,6 +3,10 @@ import com.ifsudestemg.ecommerce.example.ecommerceapi.api.dto.PagamentoParcelado
 import com.ifsudestemg.ecommerce.example.ecommerceapi.model.entity.PagamentoParcelado;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.exception.RegraNegocioException;
 import com.ifsudestemg.ecommerce.example.ecommerceapi.service.PagamentoParceladoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -20,13 +24,20 @@ public class PagamentoParceladoController {
     private final PagamentoParceladoService service;
 
     @GetMapping()
+    @ApiOperation("Obter todos os pagamento parceladoes")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Pagamento parcelado encontrado")})
     public ResponseEntity get() {
         List<PagamentoParcelado> pagamentoParcelados = service.getPagamentoParcelado();
         return ResponseEntity.ok(pagamentoParcelados.stream().map(PagamentoParceladoDTO::create).collect(Collectors.toList()));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity get(@PathVariable("id") Long id) {
+    @ApiOperation("Obter detalhes de um pagamento parcelado")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Pagamento parcelado encontrado"),
+            @ApiResponse(code = 404, message = "Pagamento parcelado não encontrado")})
+    public ResponseEntity get(@PathVariable("id") @ApiParam("Id do Pagamento parcelado") Long id) {
         Optional<PagamentoParcelado> pagamentoParcelado = service.getPagamentoParceladoById(id);
         if (!pagamentoParcelado.isPresent()) {
             return new ResponseEntity("Pagamento Parcelado não encontrado", HttpStatus.NOT_FOUND);
@@ -35,6 +46,10 @@ public class PagamentoParceladoController {
     }
 
     @PostMapping()
+    @ApiOperation("Salvar um pagamento parcelado")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Pagamento parcelado criado com sucesso"),
+            @ApiResponse(code = 400, message = "Erro ao salvar pagamento parcelado")})
     public ResponseEntity post(PagamentoParceladoDTO dto) {
         try {
             PagamentoParcelado pagamentoParcelado = converter(dto);
@@ -46,7 +61,11 @@ public class PagamentoParceladoController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity atualizar(@PathVariable("id") Long id, PagamentoParceladoDTO dto) {
+    @ApiOperation("Alterar um pagamento parcelado")
+    @ApiResponses({
+            @ApiResponse(code = 202, message = "Pagamento parcelado alterado com sucesso"),
+            @ApiResponse(code = 400, message = "Erro ao alterar pagamento parcelado")})
+    public ResponseEntity atualizar(@PathVariable("id") @ApiParam("Id do Pagamento parcelado") Long id, PagamentoParceladoDTO dto) {
         if (!service.getPagamentoParceladoById(id).isPresent()) {
             return new ResponseEntity("PagamentoParcelado não encontrado", HttpStatus.NOT_FOUND);
         }
@@ -61,7 +80,12 @@ public class PagamentoParceladoController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity excluir(@PathVariable("id") Long id) {
+    @ApiOperation("Apagar um pagamento parcelado")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Pagamento parcelado excluido com sucesso"),
+            @ApiResponse(code = 400, message = "Erro ao excluir pagamento parcelado"),
+            @ApiResponse(code = 404, message = "Pagamento parcelado não encontrado")})
+    public ResponseEntity excluir(@PathVariable("id") @ApiParam("Id do Pagamento parcelado") Long id) {
         Optional<PagamentoParcelado> pagamentoParcelado = service.getPagamentoParceladoById(id);
         if (!pagamentoParcelado.isPresent()) {
             return new ResponseEntity("PagamentoParcelado não encontrado", HttpStatus.NOT_FOUND);
